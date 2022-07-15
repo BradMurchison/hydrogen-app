d241425e1cf0219d604b825ffea53775aacb17d1
Brad Murchison <brad.murchison@shopify.com>
2022-07-15 14:29:46 +1200

    hydrogen.config.js update

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -1,18 +1,18 @@
 import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
 
 export default defineConfig({
-  shopify: {
-    defaultCountryCode: 'US',
-    defaultLanguageCode: 'EN',
+  routes: import.meta.globEager('./src/routes/**/*.server.[jt](s|sx)'),
+  shopify: () => ({
+    defaultLocale: 'en',
     storeDomain: Oxygen.env.SHOPIFY_STORE_DOMAIN,
     storefrontToken: Oxygen.env.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN,
     storefrontApiVersion: '2022-07',
-  },
+  }),
   session: CookieSessionStorage('__session', {
     path: '/',
     httpOnly: true,
-    secure: import.meta.env.PROD,
-    sameSite: 'Strict',
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
     maxAge: 60 * 60 * 24 * 30,
-  }),
+  })
 });
